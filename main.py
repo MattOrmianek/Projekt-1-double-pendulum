87dd43a839248591c13f9c0b28a6101134b21fe2
MattOrmianek <mattormianek@gmail.com>
2020-05-30 12:45:14 +0200

    Added extra plots and data input

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import matplotlib.animation as animation
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 import tkinter as tk
+import matplotlib
 
 
 def show_entry_fields():
-    
     global mass1, mass2, length1, length2, angle1, angle2, G
     length1 = float(e1.get())
     length2 = float(e2.get())
@@ -18,10 +18,8 @@ def show_entry_fields():
     mass2 = float(e4.get()) 
     angle1 = float(e5.get()) 
     angle2 = float(e6.get()) 
-    G = float(e7.get()) 
-   
-
-
+    G = float(e7.get())
+    master.destroy()
 master = tk.Tk()
 tk.Label(master, text="L1").grid(row=0)
 tk.Label(master, text="L2").grid(row=1)
@@ -44,17 +42,10 @@ e4.grid(row=3, column=1)
 e5.grid(row=4, column=1)
 e6.grid(row=5, column=1)
 e7.grid(row=6, column=1)
-
-
-
 tk.Button(master, 
           text='Show', command=show_entry_fields).grid(row=7, 
-                                                       column=1, 
-                                                       sticky=tk.W, 
-                                                       pady=4)
-
+                                column=1,sticky=tk.W,pady=4)
 master.mainloop() 
-
 
 class DoublePendulum:
     def __init__(self,
@@ -80,6 +71,64 @@ class DoublePendulum:
                         -L1 * cos(self.state[0]),
                         -L2 * cos(self.state[2])])
         return (x,y)
+    
+    def thetaandtheta(self):
+        (L1, L2, M1, M2, G) = self.params
+        
+        theta1 = self.state[0]
+        theta2 = self.state[2]
+        mtot = M1+M2
+        theta1dot = self.init_state[1] * np.pi/180
+        theta2dot = self.init_state[3] * np.pi/180
+        A = mtot * L1
+        B = M2 * L2 * np.cos(theta1 - theta2)
+        C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
+        D = L1 / L2 * np.cos(theta1-theta2)
+        E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
+
+        theta1 = (C-B*E)/(A-B*D)
+        theta2 = E - D * theta1dot
+        x=theta1
+        y=theta2
+        return (x,y)
+
+    def theta1(self,dt):
+        (L1, L2, M1, M2, G) = self.params
+        theta1 = self.state[0]
+        theta2 = self.state[2]
+        mtot = M1+M2
+        theta1dot = self.init_state[1] * np.pi/180
+        theta2dot = self.init_state[3] * np.pi/180
+        A = mtot * L1
+        B = M2 * L2 * np.cos(theta1 - theta2)
+        C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
+        D = L1 / L2 * np.cos(theta1-theta2)
+        E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
+
+        theta1 = (C-B*E)/(A-B*D)
+        theta2 = E - D * theta1dot
+        x=dt
+        y=theta1
+        return (x,y)
+
+    def theta2(self,dt):
+        (L1, L2, M1, M2, G) = self.params
+        theta1 = self.state[0]
+        theta2 = self.state[2]
+        mtot = M1+M2
+        theta1dot = self.init_state[1] * np.pi/180
+        theta2dot = self.init_state[3] * np.pi/180
+        A = mtot * L1
+        B = M2 * L2 * np.cos(theta1 - theta2)
+        C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
+        D = L1 / L2 * np.cos(theta1-theta2)
+        E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
+
+        theta1 = (C-B*E)/(A-B*D)
+        theta2 = E - D * theta1dot
+        x=dt
+        y=theta2
+        return (x,y)
 
     def energy(self):
             (L1,L2,M1,M2,G) = self.params
@@ -94,6 +143,7 @@ class DoublePendulum:
             U = G * (M1 *y[0] +M2 * y[1])
             K = 0.5 * (M1 * np.dot(vx,vx) + M2 * np.dot(vy,vy))
             return U+K
+
     def test(self,state):
         (L1,L2,M1,M2,G) = self.params
         theta1 = self.state[0]
@@ -106,11 +156,9 @@ class DoublePendulum:
         C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
         D = L1 / L2 * np.cos(theta1-theta2)
         E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
-
         theta1 = (C-B*E)/(A-B*D)
         theta2 = E - D * theta1dot
         return [theta1,theta2]
-
 
     def dstate_dt(self,state,t):
         (M1,M2,L1,L2,G)=self.params
@@ -132,23 +180,37 @@ class DoublePendulum:
                 - (M1 + M2) * L1 * state[1] * state[1] * sin_delta
                 - (M1 + M2) * G * sin(state[2])) / den2
 
-        global temp
-        temp = dydx
         return dydx
-
     def step(self,dt):
         self.state = integrate.odeint(self.dstate_dt, self.state, [0,dt])[1]
         self.time_elapsed += dt
 
+if length1 + length2 <=2:
+    lim1 = -2
+    lim2 = 2
+elif length1 + length2 <=4:
+    lim1 = -4
+    lim2 = 4
+elif length1 + length2 <=8:
+    lim1 = -8
+    lim2 = 8
+elif length1 + length2 <=16:
+    lim1 = -16
+    lim2 = 16
+elif length1 + length2 <=32:
+    lim1 = -32
+    lim2 = 32
+elif length1 + length2 >32:
+    lim1 = -100
+    lim2 = 100
 
 pendulum = DoublePendulum([angle1,0.0,angle2,0.0],length1,length2,mass1,mass2,G)
-
 dt = 1./30 #fps
 
-
 fig = plt.figure()
+
 ax = fig.add_subplot(111,aspect='equal', autoscale_on=False,
-                    xlim=(-2,2),ylim=(-2,2),alpha=0.5)
+                    xlim=(lim1,lim2),ylim=(lim1,lim2),alpha=0.5)
 
 ax.grid()
 line, = ax.plot([],[],'o-',lw=2)
@@ -171,7 +233,6 @@ def animate(i):
     pendulum.test(dt)
     return line, time_text, energy_text
 
-
 from time import time
 t0 = time()
 animate(0)
@@ -182,102 +243,7 @@ ani = animation.FuncAnimation(fig,animate,frames=150,
                             interval=interval, blit=True, init_func=init)
 
 
-class DoublePendulum2:
-    def __init__(self,
-                init_state = [120,0,-20,0],
-                L1 = .5, #length of pendulum 1 in m
-                L2 = .5, #length of pendulum 2 in m
-                M1 = 1.0, #mass of pendulum 1 in kg
-                M2 = 2.0, # mass of pendulum 2 in kg
-                G = 9.8, # acceleration due to gravity, m/s^2
-                origin=(0,0)):
-        self.init_state = np.asarray(init_state,dtype='float')
-        self.params = (L1,L2,M1,M2,G)
-        self.origin = origin
-        self.time_elapsed = 0
-        self.state = self.init_state * np.pi/180
 
-    def position(self):
-        (L1, L2, M1, M2, G) = self.params
-        
-        theta1 = self.state[0]
-        theta2 = self.state[2]
-        mtot = M1+M2
-        theta1dot = self.init_state[1] * np.pi/180
-        theta2dot = self.init_state[3] * np.pi/180
-        A = mtot * L1
-        B = M2 * L2 * np.cos(theta1 - theta2)
-        C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
-        D = L1 / L2 * np.cos(theta1-theta2)
-        E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
-
-        theta1 = (C-B*E)/(A-B*D)
-        theta2 = E - D * theta1dot
-        x=theta1
-        y=theta2
-        return (x,y)
-
-    def energy(self):
-            (L1,L2,M1,M2,G) = self.params
-            x = np.cumsum([L1 * sin(self.state[0]),
-                        L2 * sin(self.state[2])])
-            y = np.cumsum([-L1 * cos(self.state[0]),
-                        -L2 * cos(self.state[2])])
-            vx = np.cumsum([L1*self.state[1]*cos(self.state[0]),
-                            L2*self.state[3]*cos(self.state[2])])
-            vy = np.cumsum([L1*self.state[1]*cos(self.state[0]),
-                            L2*self.state[3]*cos(self.state[2])])
-            U = G * (M1 *y[0] +M2 * y[1])
-            K = 0.5 * (M1 * np.dot(vx,vx) + M2 * np.dot(vy,vy))
-            return U+K
-    def test(self,state):
-        (L1,L2,M1,M2,G) = self.params
-        theta1 = self.state[0]
-        theta2 = self.state[2]
-        mtot = M1+M2
-        theta1dot = self.init_state[1] * np.pi/180
-        theta2dot = self.init_state[3] * np.pi/180
-        A = mtot * L1
-        B = M2 * L2 * np.cos(theta1 - theta2)
-        C = -M2 * L2 * theta2dot ** 2 * np.sin(theta1 - theta2) - mtot * G * np.sin(theta1)
-        D = L1 / L2 * np.cos(theta1-theta2)
-        E = (L1 * theta1dot ** 2 * np.sin(theta1 - theta2)- G * np.sin(theta2)) / L2
-
-        theta1 = (C-B*E)/(A-B*D)
-        theta2 = E - D * theta1dot
-        return [theta1,theta2]
-
-
-    def dstate_dt(self,state,t):
-        (M1,M2,L1,L2,G)=self.params
-        dydx = np.zeros_like(state)
-        dydx[0] = state[1]
-        dydx[2] = state[3]
-
-        cos_delta = cos(state[2] - state[0])
-        sin_delta = sin(state[2] - state[0])
-
-        den1 = (M1 + M2)  * L1 - M2 * L1 * cos_delta * cos_delta
-        dydx[1] = (M2 * L1 * state[1] * state[1] * sin_delta * cos_delta
-                + M2 * G * sin(state[2]) * cos_delta
-                + M2 * L2 * state[3] * state[3] * sin_delta 
-                - (M1+M2) * G * sin(state[0])) / den1
-        den2 = (L2 / L1) * den1
-        dydx[3] = (-M2 * L2 * state[3] * state[3] * sin_delta * cos_delta
-                + (M1 + M2) * G * sin(state[0]) * cos_delta
-                - (M1 + M2) * L1 * state[1] * state[1] * sin_delta
-                - (M1 + M2) * G * sin(state[2])) / den2
-
-        
-        return dydx
-
-    def step(self,dt):
-        self.state = integrate.odeint(self.dstate_dt, self.state, [0,dt])[1]
-        self.time_elapsed += dt
-    
-pendulum2 = DoublePendulum2([angle1,0.0,angle2,0.0],length1,length2,mass1,mass2,G)
-
-dt = 1./30 #fps
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111,aspect='equal', autoscale_on=False,
@@ -295,23 +261,76 @@ def init2():
     return line2, time_text2, energy_text2
 
 def animate2(i):
-    global pendulum2, dt
-    pendulum2.step(dt)
-    line2.set_data(*pendulum2.position())
-    time_text2.set_text('time = %.1f' % pendulum2.time_elapsed)
-    energy_text2.set_text('energy = %.3f J' % pendulum2.energy())
-    pendulum2.test(dt)
+    global pendulum, dt
+    pendulum.step(dt)
+    line2.set_data(*pendulum.thetaandtheta())
+    time_text2.set_text('time = %.1f' % pendulum.time_elapsed)
+    energy_text2.set_text('energy = %.3f J' % pendulum.energy())
+    pendulum.test(dt)
     return line2, time_text2, energy_text2
 
-from time import time
-t0 = time()
 animate2(0)
-t1 = time()
-interval = 1000 * dt - (t1-t0)
-
 ani2 = animation.FuncAnimation(fig2,animate2,frames=150,
                             interval=interval, init_func=init2)
 
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111,aspect='equal', autoscale_on=True,
+                    xlim=(0,40),ylim=(-8,8))
+
+ax3.grid()
+line3, = ax3.plot([],[],'o-')
+time_text3 = ax3.text(0.02,0.95,'', transform=ax3.transAxes)
+energy_text3 = ax3.text(0.02,0.90,'', transform=ax3.transAxes)
+
+def init3():
+    line3.set_data([],[])
+    time_text3.set_text('')
+    energy_text3.set_text('')
+    return line3, time_text3, energy_text3
+
+def animate3(i):
+    global pendulum, dt
+    pendulum.step(dt)
+    line3.set_data(*pendulum.theta1(pendulum.time_elapsed))
+    time_text3.set_text('time = %.1f' % pendulum.time_elapsed)
+    energy_text3.set_text('energy = %.3f J' % pendulum.energy())
+    return line3, time_text3, energy_text3
+
+animate3(0)
+
+ani3 = animation.FuncAnimation(fig3,animate3,frames=150,
+                            interval=interval, init_func=init3)
+
+dt = 1./30
+
+fig4 = plt.figure()
+ax4 = fig4.add_subplot(111,aspect='equal', autoscale_on=True,
+                    xlim=(0,40),ylim=(-8,8))
+
+ax4.grid()
+line4, = ax4.plot([],[],'o-')
+time_text4 = ax4.text(0.02,0.95,'', transform=ax4.transAxes)
+energy_text4 = ax4.text(0.02,0.90,'', transform=ax4.transAxes)
+
+def init4():
+    line4.set_data([],[])
+    time_text4.set_text('')
+    energy_text4.set_text('')
+    return line4, time_text4, energy_text4
+
+def animate4(i):
+    global pendulum, dt
+    pendulum.step(dt)
+    line4.set_data(*pendulum.theta2(pendulum.time_elapsed))
+    time_text4.set_text('time = %.1f' % pendulum.time_elapsed)
+    energy_text4.set_text('energy = %.3f J' % pendulum.energy())
+    return line4, time_text4, energy_text4
+
+animate4(0)
+
+ani4 = animation.FuncAnimation(fig4,animate4,frames=150,
+                            interval=interval, init_func=init4)
+
+
+
 plt.show()
-
-
